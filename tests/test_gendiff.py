50ec85116f36ed814ee3d203414fbca687dadc92
Mikhail Egorov <pe.egorov@gmail.com>
2024-01-11 14:21:56 +0600

    fix tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,7 +3,7 @@ from gendiff.scripts.gendiff import generate_diff
 
 def test_generate_diff():
     assert generate_diff(
-        "/home/project_gendiff/python-project-50/file1.json",
-        "/home/project_gendiff/python-project-50/file2.json"
+        "python-project-50/tests/fixtures/file1.json",
+        "python-project-50/tests/fixtures/file2.json"
     ) == ("{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n"
           "  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}")
