234eea5a6db58020c2194236da118842a58908d6
Mikhail Egorov <pe.egorov@gmail.com>
2024-01-11 15:08:47 +0600

    fix test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,9 +1,12 @@
+import os
 from gendiff.scripts.gendiff import generate_diff
 
 
 def test_generate_diff():
-    assert generate_diff(
-        "/home/project_gendiff/python-project-50/tests/fixtures/file1.json",
-        "/home/project_gendiff/python-project-50/tests/fixtures/file2.json"
-    ) == ("{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n"
-          "  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}")
+    local_path = '/home/project_gendiff/python-project-50'
+    path_first_file = os.path.join(local_path, 'tests/fixtures/file1.json')
+    path_second_file = os.path.join(local_path, 'tests/fixtures/file2.json')
+    diff = ("{\n  - follow: False\n    host: hexlet.io\n"
+            "  - proxy: 123.234.53.22\n  - timeout: 50\n"
+            "  + timeout: 20\n  + verbose: True\n}")
+    assert generate_diff(path_first_file, path_second_file) == diff
