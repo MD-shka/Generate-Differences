42da9e6b38697f9234a93c6c66450c82b74496ed
Mikhail Egorov <pe.egorov@gmail.com>
2024-01-11 11:30:02 +0600

    fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,16 +3,11 @@ import argparse
 import json
 
 
-def json_load_file(path_file):
-    return json.load(open(path_file))
-
-
 def generate_diff(path_first_file, path_second_file):
     diff = {}
-    first_file = json_load_file(path_first_file)
-    second_file = json_load_file(path_second_file)
-    keys = set().union(first_file.keys(), second_file.keys())
-    # keys = set(first_file.keys()) | set(second_file.keys())
+    first_file = json.load(open(path_first_file))
+    second_file = json.load(open(path_second_file))
+    keys = set(first_file.keys()) | set(second_file.keys())
 
     for key in sorted(keys):
         value_first_file = first_file.get(key)
