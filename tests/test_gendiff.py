14a517651509ed14173c8ea58765eb84c5bbeb51
Mikhail Egorov <pe.egorov@gmail.com>
2024-01-11 14:09:13 +0600

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,4 +5,5 @@ def test_generate_diff():
     assert generate_diff(
         "/home/project_gendiff/python-project-50/file1.json",
         "/home/project_gendiff/python-project-50/file2.json"
-    ) == "{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}"
+    ) == ("{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n"
+          "  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}")
