85972494f963ef22cf5dcad9641664d6510d9993
Mikhail Egorov <pe.egorov@gmail.com>
2024-01-18 22:44:13 +0600

    fix build.py

--- gen_diff/build.py
+++ gen_diff/build.py
@@ -1,3 +1,7 @@
+def is_dict(value):
+    return isinstance(value, dict)
+
+
 def build_diff(data1, data2):
     diff = {}
 
@@ -5,40 +9,30 @@ def build_diff(data1, data2):
         value1 = data1.get(key)
         value2 = data2.get(key)
 
-        if value1 == value2:
-            if isinstance(value1, dict):
-                nested_diff = build_diff(value1, value2)
-                if nested_diff:
-                    diff[key] = {
-                        'status': 'unchanged',
-                        'value': nested_diff
-                    }
-            else:
-                diff[key] = {
-                    'status': 'unchanged',
-                    'value': value1
-                }
-        elif key in data1 and key not in data2:
+        if is_dict(value1) and is_dict(value2):
+            diff[key] = {
+                'status': 'unchanged',
+                'value': build_diff(value1, value2)
+            }
+        elif value1 == value2:
+            diff[key] = {
+                'status': 'unchanged',
+                'value': value1
+            }
+        elif key not in data2:
             diff[key] = {
                 'status': 'deleted',
                 'value': value1
             }
-        elif key not in data1 and key in data2:
+        elif key not in data1:
             diff[key] = {
                 'status': 'added',
                 'value': value2
             }
-        elif value1 != value2:
-            if isinstance(value1, dict) and isinstance(value2, dict):
-                nested_diff = build_diff(value1, value2)
-                diff[key] = {
-                    'status': 'unchanged',
-                    'value': nested_diff
-                }
-            else:
-                diff[key] = {
-                    'status': 'changed',
-                    'old_value': value1,
-                    'new_value': value2
-                }
+        else:
+            diff[key] = {
+                'status': 'changed',
+                'old_value': value1,
+                'new_value': value2
+            }
     return diff
