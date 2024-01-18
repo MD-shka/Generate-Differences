430ad6eb2285c94adb41e939077ceada4a8e228f
Mikhail Egorov <pe.egorov@gmail.com>
2024-01-19 02:43:02 +0600

    fix stylish.py

--- gen_diff/stylish.py
+++ gen_diff/stylish.py
@@ -1,3 +1,10 @@
+SPECIAL_SYMBOLS = {"unchanged": "  ", "added": "+ ", "deleted": "- "}
+
+
+def is_dict(value):
+    return isinstance(value, dict)
+
+
 def format_value(value):
     if isinstance(value, bool):
         return str(value).lower()
@@ -6,54 +13,58 @@ def format_value(value):
     return value
 
 
+def get_recursive_or_value(value, shift, key, indent, depth):
+    pattern = f"{indent}{shift}{key}: "
+    return (pattern + f"""{stylish(value, depth + 1)
+            if is_dict(value)
+            else format_value(value)}""")
+
+
 def stylish(diff, depth=1):
     indent = ' ' * (depth * 4 - 2)
     result = []
     for key, changes in diff.items():
         if isinstance(changes, dict) and 'status' in changes:
-            status = changes['status']
-            if status == 'unchanged':
-                if isinstance(changes['value'], dict):
-                    result.append(f"{indent}  {key}: "
-                                  f"{stylish(changes['value'], depth + 1)}")
-                else:
-                    result.append(f"{indent}  {key}: "
-                                  f"{format_value(changes['value'])}")
-            elif status == 'deleted':
-                if isinstance(changes['value'], dict):
-                    result.append(f"{indent}- {key}: "
-                                  f"{stylish(changes['value'], depth + 1)}")
-                else:
-                    result.append(f"{indent}- {key}: "
-                                  f"{format_value(changes['value'])}")
-            elif status == 'added':
-                if isinstance(changes['value'], dict):
-                    result.append(f"{indent}+ {key}: "
-                                  f"{stylish(changes['value'], depth + 1)}")
-                else:
-                    result.append(f"{indent}+ {key}: "
-                                  f"{format_value(changes['value'])}")
-            elif status == 'changed':
-                if 'old_value' in changes:
-                    if isinstance(changes['old_value'], dict):
-                        result.append(f"{indent}- {key}: "
-                                      f"{stylish(changes['old_value'], depth + 1)}")
-                    else:
-                        result.append(f"{indent}- {key}: "
-                                      f"{format_value(changes['old_value'])}")
-                if 'new_value' in changes:
-                    if isinstance(changes['new_value'], dict):
-                        result.append(f"{indent}+ {key}: "
-                                      f"{stylish(changes['new_value'], depth + 1)}")
-                    else:
-                        result.append(f"{indent}+ {key}: "
-                                      f"{format_value(changes['new_value'])}")
+            if changes['status'] in SPECIAL_SYMBOLS:
+                result.append(
+                    get_recursive_or_value(
+                        changes['value'],
+                        SPECIAL_SYMBOLS[changes['status']],
+                        key,
+                        indent,
+                        depth
+                    )
+                )
+            if 'old_value' in changes:
+                result.append(
+                    get_recursive_or_value(
+                        changes['old_value'],
+                        SPECIAL_SYMBOLS['deleted'],
+                        key,
+                        indent,
+                        depth
+                    )
+                )
+            if 'new_value' in changes:
+                result.append(
+                    get_recursive_or_value(
+                        changes['new_value'],
+                        SPECIAL_SYMBOLS['added'],
+                        key,
+                        indent,
+                        depth
+                    )
+                )
         else:
-            if isinstance(changes, dict):
-                result.append(f"{indent}  {key}: "
-                              f"{stylish(changes, depth + 1)}")
-            else:
-                result.append(f"{indent}  {key}: {format_value(changes)}")
+            result.append(
+                get_recursive_or_value(
+                    changes,
+                    SPECIAL_SYMBOLS['unchanged'],
+                    key,
+                    indent,
+                    depth
+                )
+            )
     str_result = "\n".join(result)
     str_result = "{\n" + str_result + "\n" + indent[:-2] + "}"
     return str_result
