8340c547b20d589ecba34ffc4e91d0697f276b45
Mikhail Egorov <pe.egorov@gmail.com>
2024-01-20 22:30:49 +0600

    add func(get_change) in plain.py

--- gen_diff/output_formats/plain.py
+++ gen_diff/output_formats/plain.py
@@ -15,23 +15,38 @@ def get_str_diff(diff):
     return "\n".join(diff)
 
 
+def get_change(path_str,
+               status,
+               value=None,
+               old_value=None,
+               new_value=None
+               ):
+    if status == 'added':
+        return f"{path_str} was added with value: {format_value(value)}"
+    elif status == 'deleted':
+        return f"{path_str} was removed"
+    elif status == 'changed':
+        return (f"{path_str} was updated. From "
+                f"{format_value(old_value)} to {format_value(new_value)}")
+
+
 def plain(diff, path_key=None):
     result = []
     if path_key is None:
         path_key = []
+
     for key, changes in diff.items():
         current_path = path_key + [key]
         path_str = f"Property '{'.'.join(current_path)}'"
-        if is_dict(changes) and 'status' in changes:
-            if changes['status'] == 'added':
-                result.append(f"{path_str} was added with value: "
-                              f"{format_value(changes['value'])}")
-            elif changes['status'] == 'deleted':
-                result.append(f"{path_str} was removed")
-            elif is_dict(changes.get('value')):
-                result.append(plain(changes['value'], current_path))
-            elif 'old_value' in changes and 'new_value' in changes:
-                result.append(f"{path_str} was updated. From "
-                              f"{format_value(changes['old_value'])} to "
-                              f"{format_value(changes['new_value'])}")
+        change = get_change(
+            path_str,
+            changes['status'],
+            changes.get('value'),
+            changes.get('old_value'),
+            changes.get('new_value')
+        )
+        if change:
+            result.append(change)
+        elif is_dict(changes.get('value')):
+            result.append(plain(changes['value'], current_path))
     return get_str_diff(result)
