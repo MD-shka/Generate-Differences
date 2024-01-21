6823e5b759d54c6dac38b9af5d564b260224d147
Mikhail Egorov <pe.egorov@gmail.com>
2024-01-21 17:04:26 +0600

    fix imports

---
+++ gendiff/__init__.py
@@ -0,0 +1,6 @@
+from scripts import gen_diff
+from output_formats import format_json, plain, stylish
+from gen_diff import build, parser
+
+
+__all__ = ("gen_diff", "stylish", "plain", "format_json", "build", "parser")
