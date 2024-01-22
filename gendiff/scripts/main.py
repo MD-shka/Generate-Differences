bb1cbdfbdd977a6e3041fba572102f41a0e9eee6
Mikhail Egorov <pe.egorov@gmail.com>
2024-01-22 16:58:49 +0600

    fix+ code review part 1/2

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -5,7 +5,7 @@ from gendiff import generate_diff, parse_args
 def main():
     path_first_file, path_second_file, format_name = parse_args.parse_args()
     print(
-        generate_diff.generate_diff(
+        generate_diff(
             path_first_file,
             path_second_file,
             format_name)
